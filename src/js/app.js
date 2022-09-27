8b7078eb27ef6d8032146c687b0d40b43df78045
Mauro Vicens <blackeagledhs@gmail.com>
2022-09-27 00:24:04 -0300

    Subiendo juego del ahorcado

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+ let words = ["python", "html", "javascript", "angular", "react", "java", "flask", "cobol", "oracle", "alura"];
+
+ words [0] = document.querySelector("input");
+ 
+
+ function addwords() {
+    
+ }
+
+ function keyboard(){
+    
+ }
+
+ function playgame(){
+
+ }
